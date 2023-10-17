a4db91f02690618195a0aa0fb8dee0ea0bf7958f
Your Name <simon.be47@gmail.com>
2023-10-17 19:57:16 +0200

    a

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,18 @@
-let map = L.map('map').setView([45.849998, 1.25], 13)
+// Coordonées de Limoges
+let limoges = { "lat": 45.836, "lon": 1.264 };
 
-        // tiles loading //
+// Créer la map Leaflet
+let map = L.map('map').setView([limoges.lat, limoges.lon], 13);
+
+// Ajout des tuiles OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 5,
     maxZoom: 20,
     attribution: '© OpenStreetMap'
-}).addTo(map)
+}).addTo(map);
+
+// Ajout d'un marqueur bougeable pour Limoges
+let marqueur = L.marker([limoges.lat, limoges.lon], { draggable: false }).addTo(map);
+
+// Création d'un popup pour Limoges
+marqueur.bindPopup("Vous êtes à Limoges").openPopup();
